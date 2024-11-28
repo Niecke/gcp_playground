0a725d10c8013d46d5c2a31ce316338982f892e1
Daniel Niecke <daniel.niecke@worldline.com>
2024-11-28 22:03:41 +0100

    add terraform action

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "gcp-playground-443120-tf-state"
+  }
+}
+
+provider "google" {
+  credentials = "./secrets/gcloud.json"
+  project     = "gcp-playground"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "main"
+}
